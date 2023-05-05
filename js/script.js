206982c2093cf62afc8bc3c3f5d2cdce43ed03d9
LucOliverfn <lucas.ofn@gmail.com>
2023-05-05 09:22:50 -0300

    arquivo externo javascript

---
+++ js/script.js
@@ -0,0 +1,66 @@
+///comentario de linha
+        /*
+        comentario de bloco
+        */
+
+        //declarar uma vaiavel com uma palavra reservada var
+        // var nr1 =10;
+        // var nr2 =5;
+        // var resultado;
+        // resultado = !(parseInt(nr1)%parseInt(nr2))? "PAR" : "IMPAR";
+        // console.log(`O Numero é : ${resultado}`);
+        // //imprimir as variaveis na tela
+        // console.log(nr1);
+        // console.log(nr2);
+        //realizando as operações basicas com estes valores
+        //+/-/*
+        //Realize A concatenação com o texto resultado:
+        
+        //ADIÇÃO
+        
+        // console.log('resultado Adição'+ (parseInt(nr1)+parseInt(nr2)));
+
+        // // jeito mais certo de fazer
+        // console.log(`resultado Adição: ${parseInt(nr1)+parseInt(nr2)}`);
+
+        // //Subtração
+        
+        // console.log ('resultado subutração', (parseInt(nr1)-parseInt(nr2)));
+        // console.log(`resultado subtração: ${parseInt(nr1)-parseInt(nr2)}`);
+
+        // //Multiplicação
+        
+        // console.log('resultado multiplicação'+ (parseInt(nr1)*parseInt(nr2)));
+        // console.log(`resultado multiplicação: ${parseInt(nr1)*parseInt(nr2)}`);
+
+        // //divisão
+        
+        // console.log ('resultado divisao', (parseInt(nr1)/parseInt(nr2)));
+        // console.log(`resultado divisão: ${parseInt(nr1)/parseInt(nr2)}`);
+
+        // //resto da divisão
+        
+        // console.log ('resultado resto da divisao', (parseInt(nr1)%parseInt(nr2)));
+        // console.log(`resultado resto da divisão: ${parseInt(nr1)%parseInt(nr2)}`);
+
+        //Descobrindo se um numero é par ou impar 
+        //Utilizando operador ternario(?)
+
+        
+        //regra do const
+        //1 regra = iniciar na declaração
+        // const nome3 = "violante";
+        //2 regra = Não pode ser alterado durante o fluxo do programa
+        // nome3 = "Jonathan";
+        
+        // //Diferemça entre VAR/LET/CONST
+        // var nome1 = "Beatriz";
+        // let nome2 = "Matheus";
+
+
+        // let nome= "Beatriz";
+
+        // if(nome!=""){
+        //         let nome = "Robson"
+        // }
+        // console.log
